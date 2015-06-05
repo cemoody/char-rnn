76375f5badbed20c125bb541b99b3182b4762466
Christopher Moody <chrisemoody@gmail.com>
2015-06-05 17:18:52 +0000

    Added stylist note data collection

---
+++ data/stylist_notes_01/get_input.sh
@@ -0,0 +1,1 @@
+ psql -f input.sql --host aa-dw.ceqzd6qj6kx3.us-east-1.redshift.amazonaws.com  --port 5439 --user moody --dbname dw -t -o notes.txt
---
+++ data/stylist_notes_01/input.sql
@@ -0,0 +1,3 @@
+SELECT stylist_notes
+  FROM shipment
+ WHERE LENGTH(stylist_notes) > 0
